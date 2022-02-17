e5f26c235460ddabab34dd68857fedde167bc747
Moodix <47904885+Moodix@users.noreply.github.com>
2022-02-17 23:38:13 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -2,6 +2,8 @@ import json
 
 from PIL import Image
 from werkzeug.wrappers import Request, Response
+import pandas as pd
+import joblib
 
 from utils.image import predict_image, process_image
 from utils.model import load_model
@@ -15,15 +17,21 @@ def predict(environ, start_response):
     if not request.files:
         return Response('no file uploaded', 400)(environ, start_response)
     image_file = next(request.files.values())
-    image, inverted = process_image(Image.open(image_file))
+    testset = pd.read_csv(image_file)
+
+
 
     # The predictor must be lazily instantiated;
     # the TensorFlow graph can apparently not be shared
     # between processes.
     global model
     if not model:
-        model = load_model('model.h5')
-    prediction = predict_image(model, image, inverted)
+        model = joblib.load('model.h5')
+    one_hot_encoded_data2 = pd.get_dummies(testset, columns = ['Code'])
+    df2 = one_hot_encoded_data2[["Delay", "Code_200", "Code_201", "Code_204", "Code_302", "Code_400", "Code_404", "Code_500","Y"]]
+    X_test=df2[["Delay", "Code_200", "Code_201", "Code_204", "Code_302", "Code_400", "Code_404", "Code_500"]]
+    y_test=df2[["Y"]]
+    prediction = model.predict(X_test)
 
     # The following line allows Valohai to track endpoint predictions
     # while the model is deployed. Here we remove the full predictions
